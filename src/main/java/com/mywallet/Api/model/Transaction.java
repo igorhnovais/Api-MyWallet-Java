d86dc2e3490c20ee6ad9f009e89583ffd4f707b8
Igor Novais <igorhnovaisdriven@gmail.com>
2023-06-22 22:15:12 -0300

    feat model

---
+++ src/main/java/com/mywallet/Api/model/Transaction.java
@@ -0,0 +1,64 @@
+package com.mywallet.Api.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "testeeee")
+public class Transaction implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private double price;
+    private String status;
+    private LocalDateTime created = LocalDateTime.now();
+    private int user_id;
+
+    public long getId(){
+        return id;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public LocalDateTime getCreated(){
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created){
+        this.created = created;
+    }
+
+    public int getUser_id(){
+        return user_id;
+    }
+
+    public void setUser_id(int user_id){
+        this.user_id = user_id;
+    }
+}
