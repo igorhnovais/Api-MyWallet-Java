fda9cc27ac9cba54190ed2d9bc60cc2d11a17f0e
Igor Novais <igorhnovaisdriven@gmail.com>
2023-06-23 17:35:36 -0300

    feat: updateTransaction

--- src/main/java/com/mywallet/Api/controller/TransactionsController.java
+++ src/main/java/com/mywallet/Api/controller/TransactionsController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,4 +70,10 @@ public class TransactionsController {
         return ResponseEntity.status(HttpStatus.CREATED)
         .body("criado com sucesso!");
     } 
+
+    @PutMapping("/{id}")
+    public String updateTransaction(@PathVariable Long id, @RequestBody TransactionDTO req){
+        service.update(id, req);
+        return "Atualizado";
+    }
 }
