6c0dd5e3170882fd2058e5979d2c958a7534f72f
Igor Novais <igorhnovaisdriven@gmail.com>
2023-06-23 17:46:32 -0300

    feat: deleteTransaction

--- src/main/java/com/mywallet/Api/controller/TransactionsController.java
+++ src/main/java/com/mywallet/Api/controller/TransactionsController.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mywallet.Api.dto.TransactionDTO;
@@ -75,5 +77,12 @@ public class TransactionsController {
     public String updateTransaction(@PathVariable Long id, @RequestBody TransactionDTO req){
         service.update(id, req);
         return "Atualizado";
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public String deleteTransaction(@PathVariable Long id){
+        service.delete(id);
+        return "Deletado!";
     }
 }
